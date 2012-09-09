5eafffdd6c4f67e3e7d5c81ff1dc4516eb4f26f1
Sergey V <sftp.mtuci@gmail.com>
2012-09-09 16:29:15 +0400

    factor.c: use factor.h

--- factor.c
+++ factor.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "factor.h"
+
 int main (int argc, char **argv)
 {
 	int number = atoi(argv[1]);
 	int i;
 
+	int *deviders = calloc(100, sizeof(int));
+
 	printf("%d: ", number);
-
-	for (i = 2; i <= number;) {
-		if (number % i == 0) {
-			printf("%d ", i);
-			number /= i;
-		} else {
-			i++;
-		}
-	}
-
-	putchar('\n');
+	i = factor(number, deviders);
 	
 	return 0;
 }
