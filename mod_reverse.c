9c530dd479709c0e7a41970655f4ca1cede02109
Sergey V <sftp.mtuci@gmail.com>
2012-09-10 21:47:00 +0400

    mod_reverse.c: changed to use gcd_ext()

--- mod_reverse.c
+++ mod_reverse.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "gcd.h"
+
 int main (int argc,char *argv[])
 {
 	int a = atoi(argv[1]);
 	int n = atoi(argv[2]);
 
-	int i;
+	int d, x, y;
 
-	for(i = 1; !(((a * i) % n) == 1); i++);
-	printf("%d\n", i);
+	d = gcd_ext(a, n, &x, &y);
+
+	if (d == 1) {
+		if (x < 0) {
+			x += n;
+		}
+		printf("%d\n", x);
+	} else {
+		printf("%d\n", 0);
+	}
 
 	return 0;
 }
