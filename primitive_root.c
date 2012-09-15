bec2a31690b4baf168325832ce2964b985a0c88a
Sergey V <sftp.mtuci@gmail.com>
2012-09-16 02:14:02 +0400

    primitive_root.c: use mpfr_mod.h

--- primitive_root.c
+++ primitive_root.c
@@ -6,25 +6,7 @@
 #include <mpfr.h>
 
 #include "factor.h"
-
-unsigned long int mpfr_mod(mpfr_t a, mpfr_t n)
-{
-	mpfr_t res;
-	mpfr_init2(res, 512);
-	mpfr_set_ui(res, 0, GMP_RNDD);
-
-	long int ret;
-	unsigned long num;
-
-	mpfr_remainder(a, a, n, GMP_RNDN);
-	ret = mpfr_get_si(a, GMP_RNDN);
-	num = mpfr_get_ui(n, GMP_RNDN);
-
-	if(ret < 0)
-		ret += num;
-
-	return (unsigned long int) ret;
-}
+#include "mpfr_mod.h"
 
 int main(int argc, char *argv[])
 {
