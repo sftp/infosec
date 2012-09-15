6c1b1ba14ecd31a9ba76b5556eb8d6a5a9192fb6
Sergey V <sftp.mtuci@gmail.com>
2012-09-16 01:35:43 +0400

    move mpfr_mod() to mpfr_mod.h

--- prime.c
+++ prime.c
@@ -5,24 +5,7 @@
 #include <gmp.h>
 #include <mpfr.h>
 
-unsigned long int mpfr_mod(mpfr_t a, mpfr_t n)
-{
-	mpfr_t res;
-	mpfr_init2(res, 512);
-	mpfr_set_ui(res, 0, GMP_RNDD);
-
-	long int ret;
-	unsigned long num;
-
-	mpfr_remainder(a, a, n, GMP_RNDN);
-	ret = mpfr_get_si(a, GMP_RNDN);
-	num = mpfr_get_ui(n, GMP_RNDN);
-
-	if(ret < 0)
-		ret += num;
-
-	return (unsigned long int) ret;
-}
+#include "mpfr_mod.h"
 
 int is_prime(unsigned long int n)
 {
