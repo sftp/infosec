7aa24622d38946b3e8f2dc03b078cdb068f01851
Sergey V <sftp.mtuci@gmail.com>
2012-09-16 01:38:15 +0400

    move is_prime() to mpfr_prime.h

--- prime.c
+++ prime.c
@@ -2,38 +2,8 @@
 #include <stdlib.h>
 
 #include <math.h>
-#include <gmp.h>
-#include <mpfr.h>
 
-#include "mpfr_mod.h"
-
-int is_prime(unsigned long int n)
-{
-	unsigned long int i, l, res, power;
-	mpfr_t tmp, num;
-
-	mpfr_init2(tmp, 512);
-	mpfr_set_ui(tmp, 0, GMP_RNDD);
-
-	mpfr_init2(num, 512);
-	mpfr_set_ui(num, n, GMP_RNDD);
-
-	l = (n < 10) ? n : 10;
-
-	power = (n - 1) / 2;
-
-	for(i = 2; i < l; i++) {
-		mpfr_ui_pow_ui(tmp, i, power, GMP_RNDN);
-
-		res = mpfr_mod(tmp, num);
-
-		if (!((res == 1) || (res == n - 1))) {
-			return 0;
-		}
-	}
-
-	return l - 2; /* for probability estimate */
-}
+#include "mpfr_prime.h"
 
 int main(int argc, char *argv[])
 {
